d656520cd3566771d39ccbf003fddd11e3cf6f40
Guy-S-glitch <guysolomon365@gmail.com>
2024-05-11 16:31:53 +0300

    added search input in nav bar

--- frontend/src/app/main-nav/main-nav.component.ts
+++ frontend/src/app/main-nav/main-nav.component.ts
@@ -1,26 +1,29 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { CheckoutComponent } from '../checkout/checkout.component';
-import { RouterModule } from '@angular/router';
-import { AuthenticationComponent } from '../authentication/authentication.component';
-import { ShopComponent } from '../shop/shop.component';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatBadgeModule} from '@angular/material/badge'
-import { MatMenuModule } from '@angular/material/menu';
+import { Component, inject } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { AsyncPipe, CommonModule } from "@angular/common";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { CheckoutComponent } from "../checkout/checkout.component";
+import { RouterModule } from "@angular/router";
+import { AuthenticationComponent } from "../authentication/authentication.component";
+import { ShopComponent } from "../shop/shop.component";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 @Component({
-  selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrl: './main-nav.component.scss',
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrl: "./main-nav.component.scss",
   standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -28,21 +31,24 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatBadgeModule,
     MatMenuModule,
+    MatInputModule,
+    MatFormFieldModule,
     AsyncPipe,
     MatTooltipModule,
     AuthenticationComponent,
     CheckoutComponent,
     ShopComponent,
-    
+
     RouterModule,
-  ]
+  ],
 })
 export class MainNavComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 }
