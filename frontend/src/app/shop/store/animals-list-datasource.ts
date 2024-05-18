a25ef60051b93eb92fbf7934eb1382e6b2dd0253
Guy-S-glitch <guysolomon365@gmail.com>
2024-05-18 21:02:32 +0300

    deleted slider - for now added quantity to animalBoard interface

--- frontend/src/app/shop/store/animals-list-datasource.ts
+++ frontend/src/app/shop/store/animals-list-datasource.ts
@@ -3,6 +3,7 @@ export interface animalBoard {
   species?: string;
   description?: string;
   image: string;
+  quantity: number;
 }
 export const animals: animalBoard[] = [
   {
@@ -11,6 +12,7 @@ export const animals: animalBoard[] = [
     description:
       "Aslan is a regal male lion ruling the Serengeti plains. Known for his striking golden mane and commanding roar, he stands as a symbol of strength. As the protector of his pride, Aslan ensures their safety and leads the hunt for prey like zebras and antelopes. He embodies the spirit of the wild, maintaining the balance of his ecosystem with authority.",
     image: "https://shorturl.at/bdKMO",
+    quantity: 0,
   },
   {
     name: "Zuri",
@@ -18,6 +20,7 @@ export const animals: animalBoard[] = [
     description:
       "Meet Zuri, a clever capuchin monkey in Costa Rica. She navigates the rainforest canopy with agility, using tools for foraging. Zuri's playful interactions and social bonds define her life in the lush environment, showcasing the resilience of capuchin monkeys.",
     image: "https://shorturl.at/nJY25",
+    quantity: 0,
   },
   {
     name: "Luna",
@@ -25,6 +28,7 @@ export const animals: animalBoard[] = [
     description:
       "Luna, a graceful barn owl, hunts silently at night, embodying wisdom and mystery. Her hoots add enchantment to moonlit nights, showcasing her role as a nocturnal hunter in the ecosystem. She represents the beauty and elegance of owls.",
     image: "https://shorturl.at/nFSX9",
+    quantity: 0,
   },
   {
     name: "Phoenix",
@@ -32,6 +36,7 @@ export const animals: animalBoard[] = [
     description:
       "Meet Phoenix, a stunning peacock with iridescent feathers that captivate with elegance. His magnificent display and distinctive call add magic to the surroundings, symbolizing beauty and majesty in the avian world.",
     image: "https://shorturl.at/aILM9",
+    quantity: 0,
   },
   {
     name: "Pearl",
@@ -39,6 +44,7 @@ export const animals: animalBoard[] = [
     description:
       "Meet Pearl, a gentle pigeon with soft gray feathers that flutter gracefully in the city. She symbolizes peace and resilience, scavenging for food with grace. Pearl brings tranquility to the urban landscape, embodying the harmony between nature and city life.",
     image: "https://shorturl.at/luvN2",
+    quantity: 0,
   },
   {
     name: "Arachne",
@@ -47,6 +53,7 @@ export const animals: animalBoard[] = [
       "Meet Arachne, a stealthy spider weaving intricate webs in the forest shadows. Patient and precise, she waits for prey to become ensnared in her silk masterpiece. Arachne embodies the intricate balance of nature and the artistry of spiders.",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuh_Y_RNXRw476CgACtFpY7_CCbIir_o0ceg&s",
+    quantity: 0,
   },
   {
     name: "Titan",
@@ -54,6 +61,7 @@ export const animals: animalBoard[] = [
     description:
       "Titan, a majestic tortoise in the desert, symbolizes resilience, strength, and patience. His slow pace embodies ancient wisdom and steadfastness in the reptilian world.",
     image: "https://thumbs.dreamstime.com/b/funny-tortoise-17638852.jpg?w=768",
+    quantity: 0,
   },
   {
     name: "Zara",
@@ -61,5 +69,6 @@ export const animals: animalBoard[] = [
     description:
       "Meet Zara, a graceful zebra in the savanna, known for her distinctive stripes and social nature. She symbolizes harmony and unity in the wild, embodying the spirit of cooperation and survival.",
     image: "https://shorturl.at/enrZ7",
+    quantity: 0,
   },
 ];
